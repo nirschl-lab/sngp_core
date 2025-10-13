67f32c82e4f637aed58ff46710191f93e779dcd7
Jeffrey Nirschl <jnirschl@stanford.edu>
2025-10-13 13:37:02 -0500

    chore: remove space

--- src/train.py
+++ src/train.py
@@ -60,7 +60,7 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
     if cfg.data['img_augmentations']:
         log.info(f"Augmentations <{cfg.data.img_augmentations.train._target_}>")
-        train_augmentations = hydra.utils.instantiate( cfg.data.img_augmentations.train)
+        train_augmentations = hydra.utils.instantiate(cfg.data.img_augmentations.train)
         log.info(f"Augmentations <{cfg.data.img_augmentations.test._target_}>")
         test_augmentations = hydra.utils.instantiate( cfg.data.img_augmentations.test)
 
