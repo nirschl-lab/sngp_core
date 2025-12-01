4061298b29f0137129c04b6170738ba37f37115d
umam <maheswararao@wisc.edu>
2025-12-01 14:06:40 -0600

    chore: added smoke tests for imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -10,3 +10,8 @@ def test_imports():
     import src.data # noqa: F401
     import src.models # noqa: F401
     import src.utils # noqa: F401
+    from src.models.sngp.sngp_classifier import SNGPClassifier
+    from src.models.baseline.baseline_models import BaselineClassifier
+    from src.models.lit_module_base import LitModuleBase
+    from src.models.baseline_classification_lit_module import BaselineClassificationLitModule
+    from src.models.sngp_classification_lit_module import SNGPClassificationLitModule
